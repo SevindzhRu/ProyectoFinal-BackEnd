6561f7131a530fbc2732f485b73546908056534e
Sevindzh Shukurova <shukurova.s.n@gmail.com>
2023-04-17 17:05:11 -0500

    Websockets

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const app = express()
 const PORT = 8050
 
 // HANDLEBARS
+console.log(__dirname+'/views');
 app.engine('handlebars', handlebars.engine())
 app.set('views', __dirname+'/views')
 app.set('view engine', 'handlebars')
